05c0594edcf0629f783ffcc65c45c6444f9b7702
github username <github email address>
2019-06-04 11:41:01 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -18,23 +18,22 @@ class App extends Component {
   }
 
   render() {
-    // debugger;
     return (
       <div className="App">
-          <button onClick={() => this.handleOnClickItems()}>
-            Click to change items count
+        <button onClick={() => this.handleOnClickItems()}>
+          Click to change items count
             </button>
-          <button onClick={() => this.handleOnClickUsers()}>
-            Click to change user count
+        <button onClick={() => this.handleOnClickUsers()}>
+          Click to change user count
           </button>
-          <p>{this.props.items.length}</p>
+        <p>{this.props.items.length}</p>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+  // debugger;
   return { items: state.items }
 }
 
